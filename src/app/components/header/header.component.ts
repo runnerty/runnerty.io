7afead755d87cdc4373a921da6aa6f8227675bc0
alo <alvaro.quiros@gmail.com>
2019-03-04 19:43:03 +0100

    feat: update angular version
    some minor buges fixed too 😏

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,12 @@
-import { AfterViewInit, Component, EventEmitter, Input, Output, HostListener } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'rty-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.sass']
 })
-export class HeaderComponent implements AfterViewInit {
-  scrollTop;
+export class HeaderComponent {
   @Input() menu;
 
   constructor() {}
-
-  ngAfterViewInit() { }
 }
